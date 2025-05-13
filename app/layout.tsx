e2ab12c93360f7b789e6d0ecae7b2ecc39848ad5
allansomensi <allansomensidev@proton.me>
2025-05-13 20:10:37 -0300

    init: :tada: add initial manifest and configuration files

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Tickify Web",
+  description: "Tickify front-end",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Theme>{children}</Theme>
+      </body>
+    </html>
+  );
+}
